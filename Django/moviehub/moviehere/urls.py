b1aaa3b3881a4337cf140f295aa64d5fe9ef4a1e
shamila p <lulushamila@gmail.com>
2022-10-31 16:09:41 +0530

    gitignore file added

--- Django/moviehub/moviehere/urls.py
+++ Django/moviehub/moviehere/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('list-movies', views.list_movies, name="list_movies"),
     path('add-movie', views.add_movie, name="add_movie"),
+    path('edit-movie/<int:movie_id>', views.edit_movie, name="edit_movie"),
+    path('delete-movie/<int:movie_id>', views.delete_movie, name="delete_movie"),
 ]

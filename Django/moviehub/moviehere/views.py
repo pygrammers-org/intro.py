1a4149ea78fcc6845d02b51f4f42256b3435d1c9
shamila p <lulushamila@gmail.com>
2022-10-31 16:52:59 +0530

    edit movie and delete movie operation added. Requirement file also generated

--- Django/moviehub/moviehere/views.py
+++ Django/moviehub/moviehere/views.py
@@ -22,23 +22,25 @@ def add_movie(request):
         User.objects.create(movie_name=movie, director_name=director)
         return redirect('list_movies')
 
-def edit_movie(request,movie_id):
+
+def edit_movie(request, movie_id):
     if request.method == 'GET':
-        movie=User.objects.get(id=movie_id)
+        movie = User.objects.get(id=movie_id)
         context = {'movie': movie}
-        return render(request, 'edit_movie.html',context)
+        return render(request, 'edit_movie.html', context)
     if request.method == 'POST':
         movie_name = request.POST.get('moviename')
         director = request.POST.get('director')
-        movie=User.objects.get(id=movie_id)
-        movie.movie_name=movie_name
-        movie.director_name=director
+        movie = User.objects.get(id=movie_id)
+        movie.movie_name = movie_name
+        movie.director_name = director
         movie.save()
         return redirect('list_movies')
 
-def delete_movie(request,movie_id):
-    if request.method=='POST':
-        movie=User.objects.get(id=movie_id)
+
+def delete_movie(request, movie_id):
+    if request.method == 'POST':
+        movie = User.objects.get(id=movie_id)
         print(movie)
         movie.delete()
         return redirect('list_movies')
